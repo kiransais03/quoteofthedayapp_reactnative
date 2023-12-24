af6064f460edb7ed859cec7ee7509e8f9642e688
kiransai <kiransais03@gmail.com>
2023-12-24 23:36:34 +0530

    first

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity style={styles.button}>Hi Button</TouchableOpacity>
       <StatusBar style="auto" />
+      <View>
+      <Text>Open up App.js to start working on your app!</Text>
+          {/* <Image style={imagestyles} source={require('./img.png')}/> */}
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    border : "1px solid black"
   },
+  button : {
+    backgroundColor:"grey",
+    color:"white",
+    border : "2px solid green",
+  },
+  imagestyles : {
+    width:"1000px",
+    height:"1000px"
+  }
 });
